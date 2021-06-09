0ad8890a7cb4d446cc2df414c74bf9c8704cfcac
Vinh Nguyen <vinh.nguyen@amrutainc.com>
2021-06-09 15:06:00 -0700

    main functionality done

--- api/app.py
+++ api/app.py
@@ -10,27 +10,44 @@ init_invest = 1000
 coin = ''
 years = 0
 
-@app.route('/crypto', methods=['POST', 'GET'])
-def get_form_data():
-    global init_invest, coin, years
-    if request.method == 'POST':
-        # data = request.data
-        init_invest = request.json['initInvest']
-        coin = request.json['coin']
-        years = request.json['years']
-        # print(data)
-        print(init_invest, coin, years)
-        # current_value = calculate_current_value(int(init_invest), coin, int(years))
-        return {}
-    else:
-        cg = CoinGeckoAPI()
+@app.route('/crypto', methods=['POST'])
+def calculate_value():
 
-        date = (dt.datetime.now() - relativedelta(years=years)).strftime("%d-%m-%Y")
+    # GET DATA
+    init_invest = request.json['data']['initInvest']
+    coin = request.json['data']['coinSymbol']
+    years = request.json['data']['yearsAgo']
+    print(init_invest, coin, years)
+
+    # CALL API
+    cg = CoinGeckoAPI()
+
+    # GET PAST DATA
+    # TODO: handle error when years goes past age of coin
+    try:
+        date = (dt.datetime.now() - relativedelta(years=int(years))).strftime("%d-%m-%Y")
         past_coin_data = cg.get_coin_history_by_id(id=coin, date=date)
         past_price = past_coin_data['market_data']['current_price']['usd']
+    except KeyError as e:
+        raise
 
-        current_coin_data = cg.get_coin_by_id(id=coin)
-        current_price = current_coin_data['market_data']['current_price']['usd']
-        current_value = (current_price - past_price) / past_price * init_invest
-        print(current_value)
-        return {'value': current_value}
+    # GET CURRENT DATA
+    current_coin_data = cg.get_coin_by_id(id=coin)
+    current_price = current_coin_data['market_data']['current_price']['usd']
+
+    # CALCULATE VALUE
+    current_value = ((current_price - past_price) / past_price) * int(init_invest)
+    print(current_value)
+
+    return {'value': current_value}
+
+@app.route('/coins', methods=['GET'])
+def get_coin_ids():
+
+    cg = CoinGeckoAPI()
+
+    # GET TOP 25 COINS
+    market = cg.get_coins_markets('usd')[:25]
+    coin_list = [m['id'] for m in market]
+
+    return {'coin_list': coin_list}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button, Select } from 'antd';
 import logo from './logo.svg';
-import InputBox from './InputBox';
 import './App.css';
 
 function App() {
-  const [currentValue, setCurrentValue] = useState(0);
+  const CancelToken = axios.CancelToken;
+  const source = CancelToken.source();
 
-  // useEffect(() => {
-  //   fetch('/crypto')
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     setCurrentValue(data.value);
-  //   })
-  // })
+  const [currentValue, setCurrentValue] = useState(0);
+  const [initialInvestment, setInitialInvestment] = useState(1000);
+  const [coinSymbol, setCoinSymbol] = useState("bitcoin");
+  const [yearsAgo, setYearsAgo] = useState(5);
+  const [coinList, setCoinList] = useState([])
+  const { Option } = Select;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      initInvest: initialInvestment,
+      coinSymbol: coinSymbol,
+      yearsAgo: yearsAgo,
+    }
+    console.log("Initial Investment: ", initialInvestment)
+    console.log("Coin: ", coinSymbol)
+    console.log("# of years: ", yearsAgo)
+
+    axios.post('/crypto', { data })
+      .then(res => {
+        console.log(res);
+        console.log(res.data)
+        setCurrentValue(res.data.value)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  };
+
+  const getCoinList = () => {
+    if (coinList.length === 0) {
+      console.log('getting coin list')
+      axios.get('/coins')
+        .then(res => {
+          console.log('list of Coins:', res.data.coin_list);
+          setCoinList(res.data.coin_list);
+        })
+    }
+  };
+
+  getCoinList()
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <InputBox></InputBox>
 
-        <p>You would have ${currentValue}.</p>
+    <div className="App">
+
+      <header className="App-header">
+        <div className="inputBox" >
+          <form method="post">
+            <label>If you invested $</label>
+            <input
+              name='initInvest'
+              id='initInvest'
+              className="inputBox"
+              onChange={e => setInitialInvestment(e.target.value)}
+              value={initialInvestment}
+              placeholder="Initial Investment ($)"
+              type="number"
+            />
+
+            <label> in </label>
+            {/* <Select placeholder="Select a coin"
+            onChange={e => setCoinSymbol(e.target.value)}
+            value={coinSymbol}
+            style={{ width: 200 }}>
+              <Option value="bitcoin">Bitcoin</Option>
+              <Option value="ethereum">Ethereum</Option>
+              <Option value="binancecoin">Binance Coin</Option>
+              <Option value="cardano">Cardano</Option>
+            </Select> */}
+            <select placeholder="Select a coin"
+            onChange={e => setCoinSymbol(e.target.value)}
+            value={coinSymbol}
+            style={{ width: 200 }}>
+              <option value="bitcoin">Bitcoin</option>
+              <option value="ethereum">Ethereum</option>
+              <option value="binancecoin">Binance Coin</option>
+              <option value="cardano">Cardano</option>
+            </select>
+
+            <input
+              name='years'
+              id='years'
+              className="inputBox"
+              onChange={e => setYearsAgo(e.target.value)}
+              value={yearsAgo}
+              placeholder="Years"
+              type="number"
+              min={1}
+            />
+            <label> years ago,</label>
+            <p>
+              <Button size="large" onClick={handleSubmit} type="primary">Submit</Button>
+            </p>
+          </form>
+        </div>
+
+        <p>You would have ${currentValue.toFixed(2).toLocaleString()}.</p>
       </header>
     </div>
   );
